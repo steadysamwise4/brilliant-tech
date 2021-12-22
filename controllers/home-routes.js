630518305dd7545590da2f5b5ab254029ed6fdc3
Sam Davenport <samueldavenport@att.net>
2021-12-22 08:12:47 -0600

    log in and log out functionality included with express sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,12 +32,31 @@ router.get('/', (req, res) => {
     })
     .then(dbLinkData => {
         // pass a single post object into the homepage template
-        res.render('homepage', dbLinkData[0].get({ plain: true }));
+        const links = dbLinkData.map(link => link.get({ plain: true }));
+        res.render('homepage', { links });
       })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
 });
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+      }
+    res.render('signup');
+  });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+      }
+    res.render('login');
+  });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.MY_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
